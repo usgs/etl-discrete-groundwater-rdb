9c25f72f153016f903120ca4e1636e7759cba594
ssoper <ssoper@usgs.gov>
2020-10-09 10:27:03 -0500

    now grabbing the nwis method code from the transform db, no need to join in the observation db

--- src/main/resources/sql/selectDiscreteGroundWater.sql
+++ src/main/resources/sql/selectDiscreteGroundWater.sql
@@ -6,6 +6,7 @@ select
     d.measuring_agency_code,
     d.level_accuracy_code,
     d.site_status_code,
+    d.measurement_method_code,
     d.date_measured,
     d.date_measured_raw,
     d.date_time_accuracy_code,
@@ -13,15 +14,12 @@ select
     d.time_measured_utc,
     d.approval_status_code,
     d.parameter_code,
-    d.display_result,
-    c.meth_cd measurement_method_code
+    d.display_result
   from nwis.discrete_ground_water_aqts d
   join nwis.monitoring_location m
     on m.monitoring_location_id = d.monitoring_location_id
   join nwis.nwis_district_cds_by_host h
     on m.district_cd = h.district_cd
-  join nwis.meth_with_cit c
-    on d.measurement_method = c.meth_nm
  where h.host_name not like 'nwisd%'
    and h.state_name in (:states)
  order by h.state_name, d.monitoring_location_identifier, d.date_measured_raw
